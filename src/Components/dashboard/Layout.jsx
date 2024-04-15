f66ad971fad5e7a16ab9f58b48194cadf9dc2e75
IbiminaFG <gogoibimina@gmail.com>
2024-04-15 19:36:13 +0100

    adjustments

--- src/Components/dashboard/Layout.jsx
+++ src/Components/dashboard/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = ({ sideBarToggle, children }) => {
   return (
     <div>
       <div className="flex w-full">
-        <div className="screen_height w-[90%] mx-auto">
+        <div className="screen_height flex flex-col w-[90%] mx-auto">
           <div className="w-full">
             <Header sideBarToggle={sideBarToggle} />
           </div>
